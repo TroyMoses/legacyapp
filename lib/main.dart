cdd2c59a427b7f8f48a46f9732cc672256dd3316
TroyMoses <mugabimoses07@gmail.com>
2024-07-18 06:03:26 +0800

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() {
- 
-}
+void main() {}
 
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Troy Legacy App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Center(
+          child: Text('Hello World'),
+        ),);
+  }
+}
